9591bc7258c576de7cf1ee05d87c723fb03d07b7
codersimone <97359905+codersimone@users.noreply.github.com>
2022-07-11 00:26:01 +0300

    убраны фигурные скобки и ключевое слово return

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let arr = ['js', 'css', 'html'];
 alert(arr[0]);
 
 arr = [0, 1, false, 2, undefined, '', 3, null];
-let newArray = arr.filter(element => {return element});
+let newArray = arr.filter(element => element);
 alert(newArray);
